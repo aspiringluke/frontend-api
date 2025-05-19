81d27ab2c88dbdca5cbe872f30bd48e5487f6e63
Lucas <l.paulino@sou.unifeob.edu.br>
2025-05-19 16:29:38 -0300

    fix: API real é chamada na página de conta

--- src/js/conta_script.js
+++ src/js/conta_script.js
@@ -1,39 +1,47 @@
-const apiUrl = "https://jsonplaceholder.typicode.com/users/1";
+const apiUrl = "http://localhost:4040/users";
+const userId = localStorage.getItem('userId');
 
 async function carregarPerfil() {
-    try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-
-    document.getElementById("nome").textContent = data.name;
-    document.getElementById("email").textContent = data.email;
-    } catch (error) {
-    console.log("Erro ao carregar os dados:", error);
-    }
+    const token = localStorage.getItem('token');
+    fetch(apiUrl+"/"+userId, {
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "bearer "+token
+        }
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById("nome").textContent = data.values.nome;
+        document.getElementById("email").textContent = data.values.email;
+    })
+    .catch(error => {
+        console.error("Erro ao carregar os dados:", error);
+    });
 }
 
 async function atualizarPerfil() {
-    try {
-    const response = await fetch(apiUrl, {
+    const token = localStorage.getItem('token');
+    await fetch(apiUrl+"/"+userId, {
         method: "PUT",
         headers: {
-        "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Authorization": "bearer "+token
         },
         body: JSON.stringify({
-        name: "Novo Nome",
-        email: "novoemail@email.com"
+            name: "Novo Nome",
+            email: "novoemail@email.com"
         })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log("Perfil atualizado:", data);
+        document.getElementById("nome").textContent = data.name;
+        document.getElementById("email").textContent = data.email;
+    })
+    .catch (error => {
+        console.log("Erro ao atualizar o perfil:", error);
     });
-
-    const data = await response.json();
-    console.log("Perfil atualizado:", data);
-
-    document.getElementById("nome").textContent = data.name;
-    document.getElementById("email").textContent = data.email;
-
-    } catch (error) {
-    console.log("Erro ao atualizar o perfil:", error);
-    }
 }
 
 carregarPerfil();
